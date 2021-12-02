44bdbffc50dfbafbd4d4421dffae8a4c0d5c6972
Metod Medja <metod@plume.com>
2021-12-02 12:34:31 +0100

    Day 01: Sonar Sweep (Part B)

--- src/challenge/day_01.rs
+++ src/challenge/day_01.rs
@@ -8,3 +8,19 @@ pub async fn part_01() -> anyhow::Result<usize> {
 
     Ok(depths.windows(2).filter(|x| x[1] > x[0]).count())
 }
+
+pub async fn part_02() -> anyhow::Result<usize> {
+    let input = crate::http::get("https://adventofcode.com/2021/day/1/input").await?;
+
+    let depths = input
+        .lines()
+        .map(|line| line.parse())
+        .collect::<Result<Vec<u32>, _>>()?;
+
+    let averages = depths
+        .windows(3)
+        .map(|depths| depths.iter().sum())
+        .collect::<Vec<u32>>();
+
+    Ok(averages.windows(2).filter(|x| x[1] > x[0]).count())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod http;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Sonar Sweep: {}", challenge::day_01::part_01().await?);
+    //println!("Sonar Sweep: {}", challenge::day_01::part_01().await?);
+    println!("Sonar Sweep: {}", challenge::day_01::part_02().await?);
     Ok(())
 }

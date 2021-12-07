6d5cb19d285c2403f82d0e219e154e87cd6b1541
Metod Medja <metod@plume.com>
2021-12-07 07:03:52 +0100

    Optimize lanternfish counting

--- src/challenge/day_06.rs
+++ src/challenge/day_06.rs
@@ -1,3 +1,6 @@
+const COUNTERS_LEN: usize = 9;
+const DAYS_TO_REPRODUCE: usize = 7;
+
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
     simulate(input[0], 80)
 }
@@ -7,11 +10,7 @@ pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
 }
 
 fn simulate(input: &str, days: usize) -> anyhow::Result<u64> {
-    let mut counters = Counters::new();
-
-    for number in input.split(',') {
-        counters.add(number.parse()?);
-    }
+    let mut counters = Counters::try_from(input)?;
 
     for _ in 0..days {
         counters.advance();
@@ -20,23 +19,29 @@ fn simulate(input: &str, days: usize) -> anyhow::Result<u64> {
     Ok(counters.count())
 }
 
-struct Counters([u64; 9]);
+struct Counters(usize, [u64; COUNTERS_LEN]);
 
 impl Counters {
-    fn new() -> Self {
-        Counters([0u64; 9])
-    }
-
-    fn add(&mut self, counter: u8) {
-        self.0[counter as usize] += 1;
-    }
-
     fn advance(&mut self) {
-        self.0.rotate_left(1);
-        self.0[6] += self.0[8];
+        self.1[(self.0 + DAYS_TO_REPRODUCE) % COUNTERS_LEN] += self.1[self.0];
+        self.0 = (self.0 + 1) % COUNTERS_LEN;
     }
 
     fn count(&self) -> u64 {
-        self.0.iter().sum()
+        self.1.iter().sum()
+    }
+}
+
+impl TryFrom<&str> for Counters {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut counters = [0u64; COUNTERS_LEN];
+
+        for number in value.split(',') {
+            counters[number.parse::<usize>()?] += 1;
+        }
+
+        Ok(Counters(0, counters))
     }
 }

a04cf1c2718a6eb491298d5aafb3de2951edece6
Metod Medja <metod@medja.net>
2021-12-09 14:57:12 +0100

    Remove map padding

--- src/challenge/day_09.rs
+++ src/challenge/day_09.rs
@@ -1,39 +1,98 @@
-const MAX_DEPTH: u8 = 10;
+const MAX_DEPTH: u8 = 9;
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let width = input[0].len();
-    let height = input.len();
-    let stride = width + 2;
-
-    let mut depths = Vec::<u8>::with_capacity(stride * (height + 2));
-
-    depths.extend(std::iter::repeat(MAX_DEPTH).take(stride));
-
-    for line in input {
-        depths.push(MAX_DEPTH);
-        depths.extend(line.bytes().map(|byte| byte - b'0'));
-        depths.push(MAX_DEPTH);
-    }
-
-    depths.extend(std::iter::repeat(MAX_DEPTH).take(stride));
-
+    let map = Map::new(input);
     let mut risk_level = 0usize;
 
-    for y in 1..=height {
-        for x in 1..=width {
-            let i = x + y * stride;
-            let depth = depths[i];
+    for y in 0..map.height() {
+        for x in 0..map.width() {
+            let depth = map.get(x, y);
 
-            let is_basin = depth < depths[i - stride]
-                && depth < depths[i + stride]
-                && depth < depths[i - 1]
-                && depth < depths[i + 1];
+            let is_lowest = depth < map.up(x, y)
+                && depth < map.down(x, y)
+                && depth < map.left(x, y)
+                && depth < map.right(x, y);
 
-            if is_basin {
+            if is_lowest {
                 risk_level += depth as usize + 1;
             }
         }
     }
 
     Ok(risk_level)
+}
+
+struct Map {
+    width: usize,
+    height: usize,
+    depths: Vec<u8>,
+}
+
+impl Map {
+    fn new(input: &[&str]) -> Map {
+        let width = input[0].len();
+        let height = input.len();
+
+        let values = input
+            .iter()
+            .flat_map(|line| line.bytes())
+            .map(|byte| byte - b'0');
+
+        let mut depths = Vec::<u8>::with_capacity(width * height);
+        depths.extend(values);
+
+        Map {
+            width,
+            height,
+            depths,
+        }
+    }
+
+    fn width(&self) -> usize {
+        self.width
+    }
+
+    fn height(&self) -> usize {
+        self.height
+    }
+
+    fn get(&self, x: usize, y: usize) -> u8 {
+        self.depths[(x + y * self.width)]
+    }
+
+    fn up(&self, x: usize, y: usize) -> u8 {
+        if y == 0 {
+            MAX_DEPTH
+        } else {
+            self.depths[(x + (y - 1) * self.width)]
+        }
+    }
+
+    fn down(&self, x: usize, y: usize) -> u8 {
+        let y = y + 1;
+
+        if y >= self.height {
+            MAX_DEPTH
+        } else {
+            self.depths[(x + y * self.width)]
+        }
+    }
+
+    fn left(&self, x: usize, y: usize) -> u8 {
+        if x == 0 {
+            MAX_DEPTH
+        } else {
+            self.depths[((x - 1) + y * self.width)]
+        }
+    }
+
+    fn right(&self, x: usize, y: usize) -> u8 {
+        let x = x + 1;
+
+        if x >= self.width {
+            MAX_DEPTH
+        } else {
+            self.depths[(x + y * self.width)]
+        }
+    }
 }

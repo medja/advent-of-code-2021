468c62237700673dcd4909293dcc5e8436a724c5
Metod Medja <metod@medja.net>
2021-12-11 09:59:09 +0100

    Day 11: Dumbo Octopus (Part B)

--- src/challenge/day_11.rs
+++ src/challenge/day_11.rs
@@ -1,33 +1,58 @@
 const MAP_SIZE: usize = 10;
 const MAX_INDEX: usize = MAP_SIZE - 1;
+const OCTOPUS_COUNT: usize = MAP_SIZE * MAP_SIZE;
 const MAX_ENERGY: u8 = 10;
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    let mut octopuses = [0u8; MAP_SIZE * MAP_SIZE];
-
-    for (i, byte) in input.iter().flat_map(|line| line.bytes()).enumerate() {
-        octopuses[i] = byte - b'0';
-    }
-
+    let mut octopuses = parse(input);
     let mut flashes = 0usize;
 
     for _ in 0..100 {
-        for index in 0..octopuses.len() {
-            simulate(index, &mut octopuses);
-        }
-
-        for octopus in &mut octopuses {
-            if *octopus >= MAX_ENERGY {
-                *octopus = 0;
-                flashes += 1;
-            }
-        }
+        flashes += tick(&mut octopuses);
     }
 
     Ok(flashes)
 }
 
-fn simulate(index: usize, octopuses: &mut [u8; MAP_SIZE * MAP_SIZE]) {
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let mut octopuses = parse(input);
+    let mut iteration = 1usize;
+
+    while tick(&mut octopuses) != OCTOPUS_COUNT {
+        iteration += 1;
+    }
+
+    Ok(iteration)
+}
+
+fn parse(input: &[&str]) -> [u8; OCTOPUS_COUNT] {
+    let mut octopuses = [0u8; OCTOPUS_COUNT];
+
+    for (i, byte) in input.iter().flat_map(|line| line.bytes()).enumerate() {
+        octopuses[i] = byte - b'0';
+    }
+
+    octopuses
+}
+
+fn tick(octopuses: &mut [u8; OCTOPUS_COUNT]) -> usize {
+    let mut flashes = 0usize;
+
+    for index in 0..octopuses.len() {
+        simulate(index, octopuses);
+    }
+
+    for octopus in octopuses {
+        if *octopus >= MAX_ENERGY {
+            *octopus = 0;
+            flashes += 1;
+        }
+    }
+
+    flashes
+}
+
+fn simulate(index: usize, octopuses: &mut [u8; OCTOPUS_COUNT]) {
     let octopus = match octopuses.get_mut(index) {
         Some(octopus) => octopus,
         None => return,

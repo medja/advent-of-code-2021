1df11e86f5dc40a4f2b7630874081c16a2904c1b
Metod Medja <metod@plume.com>
2021-12-14 09:03:49 +0100

    Day 14: Extended Polymerization (Part B)

--- src/challenge/day_14.rs
+++ src/challenge/day_14.rs
@@ -12,40 +12,81 @@ pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
     Ok(polymer.score())
 }
 
-struct Polymer(Vec<u8>);
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let insertions = Insertions::new(&input[2..]);
+    let mut polymer = Polymer::new(input[0]);
+
+    for _ in 0..40 {
+        polymer.apply(&insertions);
+    }
+
+    Ok(polymer.score())
+}
+
+type Pair = usize;
+
+fn normalize(byte: u8) -> u8 {
+    byte - b'A'
+}
+
+fn pair(x: u8, y: u8) -> Pair {
+    x as usize + y as usize * LETTER_COUNT
+}
+
+fn split(pair: Pair) -> (u8, u8) {
+    let x = pair % LETTER_COUNT;
+    let y = pair / LETTER_COUNT;
+
+    (x as u8, y as u8)
+}
+
+struct Polymer([u64; LOOKUP_SIZE], [u64; LETTER_COUNT]);
 
 impl Polymer {
     fn new(string: &str) -> Self {
-        Polymer(string.bytes().collect())
+        let mut pairs = [0u64; LOOKUP_SIZE];
+        let mut counts = [0u64; LETTER_COUNT];
+
+        let bytes = string.as_bytes();
+
+        counts[normalize(bytes[0]) as usize] = 1;
+
+        for (&x, &y) in bytes.iter().zip(&bytes[1..]) {
+            let x = normalize(x);
+            let y = normalize(y);
+            pairs[pair(x, y)] += 1;
+            counts[y as usize] += 1;
+        }
+
+        Polymer(pairs, counts)
     }
 
     fn apply(&mut self, insertions: &Insertions) {
-        let mut bytes = Vec::with_capacity(self.0.len() * 2 - 1);
+        let mut pairs = [0u64; LOOKUP_SIZE];
 
-        bytes.push(self.0[0]);
-
-        for (&x, &y) in self.0.iter().zip(&self.0[1..]) {
-            if let Some(insertion) = insertions.lookup(x, y) {
-                bytes.push(insertion);
+        for (p, &count) in self.0.iter().enumerate() {
+            if count == 0 {
+                continue;
             }
 
-            bytes.push(y);
+            if let Some(insertion) = insertions.lookup(p) {
+                let (x, y) = split(p);
+                pairs[pair(x, insertion)] += count;
+                pairs[pair(insertion, y)] += count;
+                self.1[insertion as usize] += count;
+            } else {
+                pairs[p] += count;
+            }
         }
 
-        self.0 = bytes;
+        self.0 = pairs;
     }
 
-    fn score(&self) -> usize {
-        let mut counts = [0usize; LETTER_COUNT];
+    fn score(&self) -> u64 {
+        let mut min = u64::MAX;
+        let mut max = u64::MIN;
 
-        for byte in &self.0 {
-            counts[(byte - b'A') as usize] += 1;
-        }
-
-        let mut min = usize::MAX;
-        let mut max = usize::MIN;
-
-        for count in counts {
+        for count in self.1 {
             if count == 0 {
                 continue;
             }
@@ -71,14 +112,14 @@ impl Insertions {
 
         for line in lines {
             let line = line.as_bytes();
-            rules[(line[0] - b'A') as usize * LETTER_COUNT + (line[1] - b'A') as usize] = line[6];
+            rules[pair(normalize(line[0]), normalize(line[1]))] = normalize(line[6]);
         }
 
         Insertions(rules)
     }
 
-    fn lookup(&self, a: u8, b: u8) -> Option<u8> {
-        match self.0[(a - b'A') as usize * LETTER_COUNT + (b - b'A') as usize] {
+    fn lookup(&self, pair: Pair) -> Option<u8> {
+        match self.0[pair] {
             0 => None,
             insertion => Some(insertion),
         }

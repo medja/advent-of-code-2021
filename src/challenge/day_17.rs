a2cffd886e88a37530b2240b09c6373411d80aef
Metod Medja <metod@medja.net>
2021-12-17 10:16:32 +0100

    Day 17: Trick Shot (Part B)

--- src/challenge/day_17.rs
+++ src/challenge/day_17.rs
@@ -1,5 +1,4 @@
 use anyhow::Context;
-use std::ops::RangeInclusive;
 
 const INVALID_TARGET: &str = "Invalid target string";
 
@@ -9,10 +8,15 @@ pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
 
     let mut best_vy = 0;
 
-    for vx in 1..target.max_x() {
-        for vy in 1..max_vy {
+    for vx in (1..=target.max_x()).rev() {
+        for vy in (best_vy + 1..max_vy).rev() {
             match simulate(vx, vy, &target) {
-                Result::Hit if vy > best_vy => best_vy = vy,
+                Result::Hit => {
+                    if vy > best_vy {
+                        best_vy = vy;
+                    }
+                    break;
+                }
                 Result::OverX => break,
                 _ => {}
             }
@@ -20,6 +24,25 @@ pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
     }
 
     Ok(best_vy * (best_vy + 1) / 2)
+}
+
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let max_vy = (i16::MAX as f32).sqrt() as i16;
+    let target = parse_target(input[0])?;
+
+    let mut count = 0;
+
+    for vx in 1..=target.max_x() {
+        for vy in target.min_y()..max_vy {
+            match simulate(vx, vy, &target) {
+                Result::Hit => count += 1,
+                Result::OverX => break,
+                _ => {}
+            }
+        }
+    }
+
+    Ok(count)
 }
 
 fn parse_target(input: &str) -> anyhow::Result<Target> {
@@ -31,8 +54,10 @@ fn parse_target(input: &str) -> anyhow::Result<Target> {
     let y2 = &input[1..];
 
     Ok(Target::new(
-        x1.parse()?..=x2.parse()?,
-        y1.parse()?..=y2.parse()?,
+        x1.parse()?,
+        x2.parse()?,
+        y1.parse()?,
+        y2.parse()?,
     ))
 }
 
@@ -44,7 +69,9 @@ fn simulate(mut vx: i16, mut vy: i16, target: &Target) -> Result {
         x += vx;
         y += vy;
 
-        if let Some(result) = target.check(x, y) {
+        let result = target.check(x, y);
+
+        if !matches!(result, Result::Miss) {
             break result;
         }
 
@@ -57,31 +84,36 @@ fn simulate(mut vx: i16, mut vy: i16, target: &Target) -> Result {
 }
 
 enum Result {
+    Miss,
     Hit,
     OverX,
     OverY,
 }
 
-struct Target(RangeInclusive<i16>, RangeInclusive<i16>);
+struct Target(i16, i16, i16, i16);
 
 impl Target {
-    fn new(x: RangeInclusive<i16>, y: RangeInclusive<i16>) -> Self {
-        Target(x, y)
+    fn new(min_x: i16, max_x: i16, min_y: i16, max_y: i16) -> Self {
+        Target(min_x, max_x, min_y, max_y)
     }
 
     fn max_x(&self) -> i16 {
-        *self.0.end()
+        self.1
     }
 
-    fn check(&self, x: i16, y: i16) -> Option<Result> {
-        if self.0.contains(&x) && self.1.contains(&y) {
-            Some(Result::Hit)
-        } else if x > *self.0.end() {
-            Some(Result::OverX)
-        } else if y < *self.1.end() {
-            Some(Result::OverY)
+    fn min_y(&self) -> i16 {
+        self.2
+    }
+
+    fn check(&self, x: i16, y: i16) -> Result {
+        if x > self.1 {
+            Result::OverX
+        } else if y < self.2 {
+            Result::OverY
+        } else if x >= self.0 && y <= self.3 {
+            Result::Hit
         } else {
-            None
+            Result::Miss
         }
     }
 }

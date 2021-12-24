dc1f0ea713815c8d2df073db5f4a300af9a2af5e
Metod Medja <metod@medja.net>
2021-12-24 16:31:08 +0100

    Day 23: Amphipod (Part B)

--- src/challenge/day_23.rs
+++ src/challenge/day_23.rs
@@ -1,8 +1,8 @@
+use anyhow::anyhow;
 use std::cmp::Ordering;
 use std::collections::hash_map::Entry;
 use std::collections::{BinaryHeap, HashMap};
 
-const PLAYER_COUNT: usize = 8;
 const ROOM_COUNT: usize = 4;
 
 const ENERGY_PER_STEP: [u32; ROOM_COUNT] = [1, 10, 100, 1000];
@@ -13,20 +13,34 @@ const DOOR_POSITIONS: [usize; ROOM_COUNT] = [2, 4, 6, 8];
 const INVALID_SPOT: u8 = 254;
 const EMPTY_SPOT: u8 = 255;
 
-type PlayerPositions = [u8; PLAYER_COUNT];
-
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    Ok(Search::find_best_path(GameState::parse(input)))
+    Search::find_best_path(GameState::<8>::parse(input))
 }
 
-struct Search {
-    queue: BinaryHeap<SearchState>,
-    costs: HashMap<PlayerPositions, u32>,
-    buffer: Vec<GameState>,
+pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
+    let extended = [
+        input[0],
+        input[1],
+        input[2],
+        "  #D#C#B#A#  ",
+        "  #D#B#A#C#  ",
+        input[3],
+        input[4],
+    ];
+
+    Search::find_best_path(GameState::<16>::parse(&extended))
 }
 
-impl Search {
-    fn find_best_path(state: GameState) -> u32 {
+type PlayerPositions<const N: usize> = [u8; N];
+
+struct Search<const N: usize> {
+    queue: BinaryHeap<SearchState<N>>,
+    costs: HashMap<PlayerPositions<N>, u32>,
+    buffer: Vec<GameState<N>>,
+}
+
+impl<const N: usize> Search<N> {
+    fn find_best_path(state: GameState<N>) -> anyhow::Result<u32> {
         let search = Search {
             queue: BinaryHeap::new(),
             costs: HashMap::new(),
@@ -36,7 +50,7 @@ impl Search {
         search.solve(state)
     }
 
-    fn solve(mut self, state: GameState) -> u32 {
+    fn solve(mut self, state: GameState<N>) -> anyhow::Result<u32> {
         self.costs.insert(*state.positions(), state.cost());
         self.queue.push(SearchState::from_game_state(state));
 
@@ -47,12 +61,11 @@ impl Search {
             };
 
             if state.is_won() {
-                return state.cost();
+                return Ok(state.cost());
             }
 
             state.find_next_states(&mut self.buffer);
 
-            // Is removing one by one faster?
             for state in self.buffer.drain(..) {
                 let cost = state.cost();
 
@@ -70,53 +83,53 @@ impl Search {
             }
         }
 
-        unreachable!()
+        Err(anyhow!("Cannot find solution"))
     }
 }
 
 #[derive(Eq, PartialEq)]
-struct SearchState {
+struct SearchState<const N: usize> {
     estimate: u32,
-    positions: PlayerPositions,
+    positions: PlayerPositions<N>,
 }
 
-impl SearchState {
-    fn from_game_state(state: GameState) -> Self {
+impl<const N: usize> SearchState<N> {
+    fn from_game_state(state: GameState<N>) -> Self {
         SearchState {
             estimate: state.estimate(),
-            positions: state.into_positions(),
+            positions: *state.positions(),
         }
     }
 
-    fn positions(&self) -> &PlayerPositions {
+    fn positions(&self) -> &PlayerPositions<N> {
         &self.positions
     }
 
-    fn build_game_state(self, cost: u32) -> GameState {
+    fn build_game_state(self, cost: u32) -> GameState<N> {
         GameState::new(cost, self.positions)
     }
 }
 
-impl Ord for SearchState {
+impl<const N: usize> Ord for SearchState<N> {
     fn cmp(&self, other: &Self) -> Ordering {
         other.estimate.cmp(&self.estimate)
     }
 }
 
-impl PartialOrd for SearchState {
+impl<const N: usize> PartialOrd for SearchState<N> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
 #[derive(Eq, PartialEq)]
-struct GameState {
+struct GameState<const N: usize> {
     cost: u32,
-    positions: PlayerPositions,
+    positions: PlayerPositions<N>,
 }
 
-impl GameState {
-    fn new(cost: u32, positions: PlayerPositions) -> Self {
+impl<const N: usize> GameState<N> {
+    fn new(cost: u32, positions: PlayerPositions<N>) -> Self {
         GameState { cost, positions }
     }
 
@@ -130,127 +143,147 @@ impl GameState {
         self.cost
     }
 
-    fn positions(&self) -> &PlayerPositions {
+    fn positions(&self) -> &PlayerPositions<N> {
         &self.positions
     }
 
-    fn into_positions(self) -> PlayerPositions {
-        self.positions
-    }
-
     fn is_won(&self) -> bool {
-        (self.positions[0] == 11 || self.positions[0] == 12)
-            && (self.positions[1] == 11 || self.positions[1] == 12)
-            && (self.positions[2] == 13 || self.positions[2] == 14)
-            && (self.positions[3] == 13 || self.positions[3] == 14)
-            && (self.positions[4] == 15 || self.positions[4] == 16)
-            && (self.positions[5] == 15 || self.positions[5] == 16)
-            && (self.positions[6] == 17 || self.positions[6] == 18)
-            && (self.positions[7] == 17 || self.positions[7] == 18)
+        let room_size = N / ROOM_COUNT;
+
+        self.positions()
+            .chunks(room_size)
+            .enumerate()
+            .all(|(i, room)| {
+                let start = (HALLWAY_LENGTH + room_size * i) as u8;
+                let stop = start + room_size as u8;
+
+                room.iter()
+                    .all(|&position| position >= start && position < stop)
+            })
     }
 
-    fn find_next_states(&self, buffer: &mut Vec<GameState>) {
-        let mut positions = [EMPTY_SPOT; HALLWAY_LENGTH + PLAYER_COUNT];
+    fn find_next_states(&self, buffer: &mut Vec<Self>) {
+        let room_size = N / ROOM_COUNT;
+
+        let mut hallway_positions = [EMPTY_SPOT; HALLWAY_LENGTH];
+        let mut room_positions = [EMPTY_SPOT; N];
 
         for position in DOOR_POSITIONS {
-            positions[position] = INVALID_SPOT;
-        }
-
-        for (index, position) in self.positions.iter().enumerate() {
-            positions[*position as usize] = index as u8;
+            hallway_positions[position] = INVALID_SPOT;
         }
 
         for (index, position) in self.positions.iter().enumerate() {
             let position = *position as usize;
-            let r#type = index / 2;
+
+            if position < HALLWAY_LENGTH {
+                hallway_positions[position] = index as u8;
+            } else {
+                room_positions[position - HALLWAY_LENGTH] = index as u8;
+            }
+        }
+
+        for (index, position) in self.positions.iter().enumerate() {
+            let position = *position as usize;
+            let r#type = index / room_size;
 
             if position < HALLWAY_LENGTH {
                 // we're in the hallway
                 let door_position = DOOR_POSITIONS[r#type];
 
-                // are we next to our room?
-                if position + 1 == door_position || door_position + 1 == position {
-                    let room_position = HALLWAY_LENGTH + r#type * 2 + 1;
-                    let room = positions[HALLWAY_LENGTH + r#type * 2 + 1];
+                let mut hallway_range = if position < door_position {
+                    position + 1..door_position
+                } else {
+                    door_position + 1..position
+                };
 
-                    // can enter our room?
+                let distance = hallway_range.len();
+
+                if !hallway_range.all(|i| hallway_positions[i] >= INVALID_SPOT) {
+                    // cannot access the room
+                    continue;
+                }
+
+                let room_start = r#type * room_size;
+                let mut position = room_start + room_size;
+
+                while position > room_start {
+                    position -= 1;
+                    let room = room_positions[position];
+
                     if room == EMPTY_SPOT {
-                        buffer.push(self.move_player(3, index, room_position));
-                        continue;
-                    } else if room as usize / 2 == r#type {
-                        buffer.push(self.move_player(2, index, room_position - 1));
-                        continue;
+                        let distance = distance + 2 + position - room_start;
+                        let position = position + HALLWAY_LENGTH;
+                        buffer.push(self.move_player(distance, index, r#type, position));
+                        break;
                     }
-                }
 
-                // seems like we can't enter our room
-
-                // can get to one of the edges?
-                if position == 1 && positions[0] == EMPTY_SPOT {
-                    buffer.push(self.move_player(1, index, 0));
-                }
-
-                if position == 9 && positions[10] == EMPTY_SPOT {
-                    buffer.push(self.move_player(1, index, 10));
-                }
-
-                if position == 0 && positions[1] == EMPTY_SPOT {
-                    buffer.push(self.move_player(1, index, 1));
-                }
-
-                if position == 10 && positions[9] == EMPTY_SPOT {
-                    buffer.push(self.move_player(1, index, 9));
-                }
-
-                // what about move left (in the hallway)?
-                if position > 2 && positions[position - 2] == EMPTY_SPOT {
-                    buffer.push(self.move_player(2, index, position - 2));
-                }
-
-                // or move right?
-                if position < 8 && positions[position + 2] == EMPTY_SPOT {
-                    buffer.push(self.move_player(2, index, position + 2));
+                    if room as usize / room_size != r#type {
+                        // room is occupied by a different kind
+                        break;
+                    }
                 }
             } else {
                 // we're in a room
-                let position_in_room = position - HALLWAY_LENGTH;
-                let room = position_in_room / 2;
-                let front = position_in_room % 2 == 0;
+                let position = position - HALLWAY_LENGTH;
+                let room = position / room_size;
+                let depth = position % room_size;
+                let room_start = room * room_size;
 
-                if front {
-                    // are we in our room with our kind?
-                    if room == r#type && positions[position + 1] as usize / 2 == r#type {
-                        continue;
-                    }
-                } else {
-                    // is this our room or are we stuck?
-                    if room == r#type || positions[position - 1] != EMPTY_SPOT {
-                        continue;
-                    }
+                if room == r#type
+                    && (depth + 1..room_size)
+                        .all(|i| room_positions[room_start + i] as usize / room_size == r#type)
+                {
+                    // we're in the right room
+                    continue;
                 }
 
-                // we're in the wrong room and we can get to the door
+                if depth > 0 && (0..depth).any(|i| room_positions[room_start + i] != EMPTY_SPOT) {
+                    // we're blocked in the room
+                    continue;
+                }
+
                 let door_position = DOOR_POSITIONS[room];
-                let distance = if front { 2 } else { 3 };
+                let mut distance = 2 + depth;
 
-                if positions[door_position - 1] == EMPTY_SPOT {
-                    buffer.push(self.move_player(distance, index, door_position - 1));
+                for (i, &spot) in hallway_positions
+                    .iter()
+                    .enumerate()
+                    .take(door_position)
+                    .rev()
+                {
+                    if spot == EMPTY_SPOT {
+                        buffer.push(self.move_player(distance, index, r#type, i));
+                    } else if spot != INVALID_SPOT {
+                        break;
+                    }
+
+                    distance += 1;
                 }
 
-                if positions[door_position + 1] == EMPTY_SPOT {
-                    buffer.push(self.move_player(distance, index, door_position + 1));
+                distance = 2 + depth;
+
+                for (i, &spot) in hallway_positions.iter().enumerate().skip(door_position + 1) {
+                    if spot == EMPTY_SPOT {
+                        buffer.push(self.move_player(distance, index, r#type, i));
+                    } else if spot != INVALID_SPOT {
+                        break;
+                    }
+
+                    distance += 1;
                 }
             }
         }
     }
 
     fn estimate(&self) -> u32 {
+        let room_size = N / ROOM_COUNT;
+
         let estimate = self
             .positions
             .iter()
             .enumerate()
             .map(|(index, position)| {
-                let r#type = index / 2;
+                let r#type = index / room_size;
                 let destination = DOOR_POSITIONS[r#type];
                 let mut position = *position as usize;
 
@@ -271,8 +304,14 @@ impl GameState {
         self.cost + estimate
     }
 
-    fn move_player(&self, distance: usize, index: usize, destination: usize) -> GameState {
-        let cost = self.cost + distance as u32 * ENERGY_PER_STEP[index / 2];
+    fn move_player(
+        &self,
+        distance: usize,
+        index: usize,
+        r#type: usize,
+        destination: usize,
+    ) -> Self {
+        let cost = self.cost + distance as u32 * ENERGY_PER_STEP[r#type];
 
         let mut positions = self.positions;
         positions[index] = destination as u8;
@@ -281,16 +320,17 @@ impl GameState {
     }
 }
 
-struct GameStateBuilder {
-    positions: PlayerPositions,
+struct GameStateBuilder<const N: usize> {
+    positions: PlayerPositions<N>,
     indices: [usize; ROOM_COUNT],
     position: u8,
 }
 
-impl GameStateBuilder {
+impl<const N: usize> GameStateBuilder<N> {
     fn new() -> Self {
-        let positions = [0; PLAYER_COUNT];
-        let indices = [0, 2, 4, 6];
+        let room_size = N / ROOM_COUNT;
+        let positions = [0; N];
+        let indices = [0, room_size, 2 * room_size, 3 * room_size];
         let position = HALLWAY_LENGTH as u8;
 
         GameStateBuilder {
@@ -301,17 +341,16 @@ impl GameStateBuilder {
     }
 
     fn parse(&mut self, input: &[&str]) {
-        self.insert(input[2].as_bytes()[3]);
-        self.insert(input[3].as_bytes()[3]);
-        self.insert(input[2].as_bytes()[5]);
-        self.insert(input[3].as_bytes()[5]);
-        self.insert(input[2].as_bytes()[7]);
-        self.insert(input[3].as_bytes()[7]);
-        self.insert(input[2].as_bytes()[9]);
-        self.insert(input[3].as_bytes()[9]);
+        let room_size = N / ROOM_COUNT;
+
+        for x in 0..ROOM_COUNT {
+            for y in 0..room_size {
+                self.insert(input[2 + y].as_bytes()[3 + 2 * x]);
+            }
+        }
     }
 
-    fn build(self) -> GameState {
+    fn build(self) -> GameState<N> {
         GameState::new(0, self.positions)
     }
 
